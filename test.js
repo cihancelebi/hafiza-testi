5d1759a465346e889d3cfe0a4d5253eca4f969a1
Cihan Çelebi <76438259+cihancelebi@users.noreply.github.com>
2025-02-21 11:29:03 +0300

    Update test.js

--- test.js
+++ test.js
@@ -4,19 +4,33 @@ const images = [
     { img: "https://icons.iconarchive.com/icons/ampeross/qetto-2/256/photos-icon.png", text: "Ev" },
     { img: "https://icons.iconarchive.com/icons/zhoolego/material/256/Images-icon.png", text: "Bilgisayar" }
 ];
+
+// Diziyi karıştırma fonksiyonu
 function shuffle(array) {
     return array.sort(() => Math.random() - 0.5);
 }
+
+// Görselleri gösterme fonksiyonu
 function showImages() {
     const container = document.getElementById("test-container");
-    container.innerHTML = "";
+    container.innerHTML = ""; // Önceki içerikleri temizle
+
+    // Resimleri rastgele sıraya sokarak ekle
     shuffle(images).forEach(item => {
         const div = document.createElement("div");
-        div.innerHTML = `<img src="${item.img}" alt=""> <p>${item.text}</p>`;
+        div.innerHTML = `
+            <div style="display: inline-block; margin: 10px;">
+                <img src="${item.img}" alt="${item.text}" style="max-width: 100px; max-height: 100px;">
+                <p>${item.text}</p>
+            </div>`;
         container.appendChild(div);
     });
+
+    // 10 saniye sonra eşleştirme mesajını göster
     setTimeout(() => {
         container.innerHTML = "<p>Şimdi eşleştirme yap!</p>";
     }, 10000);
 }
+
+// Sayfa yüklendiğinde resimleri göster
 showImages();
